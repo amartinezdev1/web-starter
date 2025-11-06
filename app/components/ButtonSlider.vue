30e7259caaef19625bcf6507a05f911e1bd5fadb
stpamartinez <amartinez@stprentacar.com>
2025-11-06 08:05:56 -0500

    resolve merge of remote repository

---
+++ app/components/ButtonSlider.vue
@@ -0,0 +1,28 @@
+<template>
+	<UButton
+		class="font-bold rounded-full"
+		color="neutral"
+		variant="outline"
+	>
+		{{ props.text }}
+	</UButton>
+	<UChip
+		standalone
+		inset
+		:color="props.color"
+	/>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	text: {
+		type: String,
+		required: true,
+	},
+	color: {
+		type: String as () => 'neutral' | 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'error',
+		required: false,
+		default: 'neutral',
+	},
+});
+</script>
