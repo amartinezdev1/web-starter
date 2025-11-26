91528a126d211789ccc3018471876bd82833aa1f
amartinezdev1 <angelmarti18@icloud.com>
2025-11-26 18:32:45 -0500

    feat: update application settings and home page content to improve user experience

--- app/app.config.ts
+++ app/app.config.ts
@@ -7,15 +7,15 @@ export default defineAppConfig({
 		pageHero: {
 			slots: {
 				headline: 'w-fit px-2 py-1 mx-auto text-sm font-medium border border-white rounded-lg',
-				title: 'max-w-xl mx-auto text-4xl lg:text-7xl font-medium text-pretty tracking-tight',
-				description: 'max-w-lg mx-auto text-white text-base text-center md:text-xl font-medium leading-relaxed',
+				title: 'max-w-full md:max-w-5xl mx-auto text-4xl lg:text-7xl font-medium text-pretty tracking-tight',
+				description: 'max-w-full md:max-w-4xl mx-auto text-white text-base text-center md:text-xl font-medium leading-relaxed',
 			},
 		},
 		pageSection: {
 			slots: {
 				headline: 'w-fit px-2 py-1 mx-auto text-sm font-medium border border-white rounded-lg',
-				title: 'max-w-xl mx-auto text-4xl lg:text-6xl font-medium text-pretty tracking-tight',
-				description: 'max-w-lg mx-auto text-white text-base text-center md:text-xl font-medium leading-relaxed',
+				title: 'max-w-full md:max-w-3xl mx-auto text-4xl lg:text-6xl font-medium text-pretty tracking-tight',
+				description: 'max-w-full md:max-w-3xl mx-auto text-white text-base text-center md:text-xl font-medium leading-relaxed',
 			},
 		},
 	},
