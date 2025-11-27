510a0222aed84f1f57d197ba20316a7955ba6149
amartinezdev1 <angelmarti18@icloud.com>
2025-11-26 20:20:05 -0500

    Change app title and modify title styles

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,5 +1,5 @@
 export default defineAppConfig({
-	title: 'Web Starter',
+	title: 'Bit Flow',
 	theme: {
 		dark: true,
 	},
@@ -7,14 +7,14 @@ export default defineAppConfig({
 		pageHero: {
 			slots: {
 				headline: 'w-fit px-2 py-1 mx-auto text-sm font-medium border border-white rounded-lg',
-				title: 'max-w-full md:max-w-5xl mx-auto text-4xl lg:text-7xl font-medium text-pretty tracking-tight',
+				title: 'max-w-full md:max-w-5xl mx-auto text-4xl md:text-6xl lg:text-7xl font-medium text-pretty tracking-tight',
 				description: 'max-w-full md:max-w-4xl mx-auto text-white text-base text-center md:text-xl font-medium leading-relaxed',
 			},
 		},
 		pageSection: {
 			slots: {
 				headline: 'w-fit px-2 py-1 mx-auto text-sm font-medium border border-white rounded-lg',
-				title: 'max-w-full md:max-w-2xl mx-auto text-4xl lg:text-6xl font-medium text-pretty tracking-tight',
+				title: 'max-w-full md:max-w-xl lg:max-w-2xl mx-auto text-4xl md:text-5xl lg:text-6xl font-medium text-pretty tracking-tight',
 				description: 'max-w-full md:max-w-4xl mx-auto text-white text-base text-center md:text-xl font-medium leading-relaxed',
 			},
 		},
