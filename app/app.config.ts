cde8fda678ffb32fc43fa8b3451492d75b93e10d
Angel Martinez <128103072+amartinezdev1@users.noreply.github.com>
2025-11-26 20:10:53 -0500

    Change title font size to 4xl
    Updated title font size in pageSection slots.

--- app/app.config.ts
+++ app/app.config.ts
@@ -14,7 +14,7 @@ export default defineAppConfig({
 		pageSection: {
 			slots: {
 				headline: 'w-fit px-2 py-1 mx-auto text-sm font-medium border border-white rounded-lg',
-				title: 'max-w-full md:max-w-2xl mx-auto text-3xl lg:text-6xl font-medium text-pretty tracking-tight',
+				title: 'max-w-full md:max-w-2xl mx-auto text-4xl lg:text-6xl font-medium text-pretty tracking-tight',
 				description: 'max-w-full md:max-w-4xl mx-auto text-white text-base text-center md:text-xl font-medium leading-relaxed',
 			},
 		},
